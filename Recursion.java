ac1b0bd07621611bf83ea5046a82f6d2303cbbf4
ilikeflex <ilikeflex@yahoo.com>
2022-01-09 12:41:42 -0500

    Update Recursion.java
    Added algo PDI for printing 5,4,3,2,1,1,2,3,4,5

--- Recursion.java
+++ Recursion.java
@@ -9,12 +9,13 @@ public class Recursion {
 	}
 	
 	public static void main(String args[]){
-		printIntegers(5);
-		PD(5);
+		//printIntegers(5);
+		//PD(5);
+		//PI(5);
+		PDI(5);
 	}
 	
 	public static int printIntegers(int x){
-		
 		if(x == 0) return -1;
 		else {
 			print(x);
@@ -24,11 +25,25 @@ public class Recursion {
 	
 	
 	public static void PD(int x){
-		
 		if(x == 0) return ;
 		print(x);
-		printIntegers(x-1);
-		
+		PD(x-1);
+	}
+	
+	public static void PI(int x){
+		if(x == 0) return ;
+		PI(x-1);		
+		print(x);
+	}
+	
+	
+	public static void PDI(int x){
+		if(x == 0) return ;
+		print(x);
+		PDI(x-1);
+				
+		print(x);
+		if(x == 5) return ;		
 	}
 	
 }
