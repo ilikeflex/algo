437a5fad34e4348ed4eb81af1cb222339dbc27cc
Rajan <ilikeflex@yahoo.com>
2022-01-24 23:16:58 -0500

    Added Input Parameters

--- StringToInteger.java
+++ StringToInteger.java
@@ -1,42 +1,57 @@
 import java.lang.Integer;
+import java.util.Scanner;
 
 public class StringToInteger {
 
 	public static void main(String[] args){
-		System.out.println("Hello World");
-		System.out.println(stringToInteger("98752"));
+		
+		//System.out.println(convertStringtoInteger("98752"));
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		System.out.println(convertStringtoInteger(input));
 	}
 		
 	static int stringToInteger(String input){
-		
-		//System.out.println("First Statement input="+input+",input.lenght="+input.length());
 	
 		if(input.length() == 0)
 		{
 			return 0;
 		}
 
-		
-		//int low = 0;
-		//int high = input.length() - 1;
 		int total = 0;
 		
 		for(int x=0,y=input.length()-1; x<1; ++x,--y){ //!!!! Loop is running only once !!!!
 			String currentChar = input.substring(x,x+1); // first is inclusive and second is exclusive
 			String ros = input.substring(x+1); // It can also be written as String ros = input.substring(x+1);
-			//System.out.println("Match 1 currentChar="+currentChar+",ros="+ros+",x="+x);
+			
 			int currentCharAsInt = Integer.parseInt(currentChar);
 			int currentValue =  currentCharAsInt * (int)Math.pow(10, y);
-			//System.out.println("Befor Recursive Loop x="+x+",currentChar="+currentChar+",ros="+ ros + ",currentValue="+currentValue+",total="+total);
+			
 			int prevValue = stringToInteger(ros);
-			//System.out.println("Mid Recursive Loop prevValue=" + prevValue);
+			
 			total = currentValue + prevValue;
-			//System.out.println("Match 2 currentChar="+currentChar+",ros="+ros+",x="+x);
-			//System.out.println("After Recurcive currentValue="+currentValue+"total="+total+",input="+input);
 		}
 		
 		return total;
 	}
+	
+	
+	static int convertStringtoInteger(String input){
+	
+		int result = -1;
+		String currentChar = input.substring(0,1); // getFirstCharacter
+		
+		//if( currentChar == "-") { // This does not work because it checks for address space
+		if( currentChar.equals("-") ) {
+			String ros = input.substring(1);	
+			result = -1 * stringToInteger(ros);
+		}
+		else
+			result = stringToInteger(input);
+		
+		return result;
+
+	}	
 	
 	static int stringToIntegerWithoutRecursion(String input){
 				
